351afd5f56ae7d1181b12eaea53f647df5f923cc
adit450 <43623502+adit450@users.noreply.github.com>
2019-04-20 08:08:16 -0400

    Update UserService.java

--- Development/MealPlanner/src/main/java/RachlinBabies/Service/UserService.java
+++ Development/MealPlanner/src/main/java/RachlinBabies/Service/UserService.java
@@ -4,14 +4,12 @@ import RachlinBabies.Model.User;
 
 import RachlinBabies.Utils.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 import java.util.logging.Level;
 
 public class UserService extends Service<User> implements UserDao{
+
 
   /**
    * To be used when the user logs in.  Sets the session user Id so that all queries
@@ -21,8 +19,10 @@ public class UserService extends Service<User> implements UserDao{
    * @param id the id of the logged in user.
    */
   private static void setUser(int id) {
-    userId = id;
+    userId = 1;
   }
+
+
 
   @Override
   public boolean create(User toInsert) {
@@ -114,4 +114,87 @@ public class UserService extends Service<User> implements UserDao{
     }
     return false;
   }
+
+  @Override
+  public boolean followUser(int id) {
+    String query = "INSERT INTO follow(follower_id, followwee_id)" +
+            "VALUES (?,?)";
+    Connection connection = DatabaseConnection.getConnection();
+      try (PreparedStatement stmt = connection.prepareStatement(query,
+              Statement.RETURN_GENERATED_KEYS)) {
+        stmt.setInt(1, 1);
+        stmt.setInt(2, id);
+        stmt.executeUpdate();
+
+      } catch(SQLException e) {
+        LOGGER.log(Level.SEVERE, e.getMessage(), e);
+      } finally {
+        DatabaseConnection.closeConnection(connection);
+      }
+      return true;
+
+  }
+
+  @Override
+  public boolean unfollowUser(int id) {
+    String query = "DELETE FROM follow WHERE follower_id = userid and followee_id = id";
+
+    Connection connection = DatabaseConnection.getConnection();
+    try (PreparedStatement stmt = connection.prepareStatement(query,
+            Statement.RETURN_GENERATED_KEYS)) {
+      stmt.setInt(1, id);
+      stmt.setInt(2, userId);
+      stmt.executeUpdate();
+
+    } catch (SQLException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+    } finally {
+      DatabaseConnection.closeConnection(connection);
+    }
+      return true;
+  }
+
+  @Override
+  public List<User> getFollowers() {
+    List<User> followers = null;
+
+    String query = "Select * from user where user_id in (Select follower_id from follow where followee_id = ?)";
+
+    Connection connection = DatabaseConnection.getConnection();
+    try (PreparedStatement stmt = connection.prepareStatement(query)) {
+      stmt.setInt(1, 1);
+      try (ResultSet rs = stmt.executeQuery()) {
+        followers = convertList(rs);
+      }
+
+    } catch (SQLException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+    } finally {
+      DatabaseConnection.closeConnection(connection);
+    }
+    return followers;
+  }
+
+  @Override
+  public List<User> getFollowing() {
+    List<User> following = null;
+
+    String query = "Select * from user where user_id in (Select followee_id from follow where follower_id = ?)";
+
+    Connection connection = DatabaseConnection.getConnection();
+    try (PreparedStatement stmt = connection.prepareStatement(query)) {
+      stmt.setInt(1, 1);
+      try (ResultSet rs = stmt.executeQuery()) {
+        following = convertList(rs);
+      }
+
+    } catch (SQLException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+    } finally {
+      DatabaseConnection.closeConnection(connection);
+    }
+    return following;
+  }
 }
+
+

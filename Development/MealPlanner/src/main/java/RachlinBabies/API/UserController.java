6609d42a1160eae8a512eff2733d534864bad1b6
adit450 <43623502+adit450@users.noreply.github.com>
2019-04-20 07:58:25 -0400

    Update UserController.java

--- Development/MealPlanner/src/main/java/RachlinBabies/API/UserController.java
+++ Development/MealPlanner/src/main/java/RachlinBabies/API/UserController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import RachlinBabies.Model.User;
@@ -15,9 +16,8 @@ import RachlinBabies.Utils.ResponseMessage;
 
 import static RachlinBabies.Utils.JsonUtil.dateGson;
 import static RachlinBabies.Utils.JsonUtil.json;
-import static spark.Spark.before;
-import static spark.Spark.options;
-import static spark.Spark.post;
+import static spark.Spark.*;
+
 
 class UserController {
 
@@ -42,9 +42,41 @@ class UserController {
                     return "OK";
                 });
 
+        get("/followers/", (req, res) -> {
+            List<User> followers = userService.getFollowers();
+            if (followers != null) {
+                return followers;
+            }
+            res.status(404);
+            return new ResponseMessage("This user doesn't exist");
+        }, json());
+
+        get("/following/", (req, res) -> {
+
+            List<User> following = userService.getFollowing();
+            if (following != null) {
+                return following;
+            }
+            res.status(404);
+            return new ResponseMessage("This user doesn't exist");
+        }, json());
+
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
-        
+
+
+         post("/follow/:id", (req, res) -> {
+             int followeeId = Integer.parseInt(req.params(":id"));
+             userService.followUser(followeeId);
+             if (userService.followUser(followeeId)) {
+                 return new ResponseMessage(String.format("User %d followed", followeeId));
+             }
+             res.status(400);
+             return new ResponseMessage(String.format("Could not follow user %d", followeeId));
+         }, json());
+
+
         post("/register", (req, res) -> {
+
             User usr = dateGson().fromJson(req.body(), User.class);
             if (userService.create(usr)) {
                 return new ResponseMessage("User successfully inserted");
@@ -65,7 +97,7 @@ class UserController {
                 // convert JSON string to Map
                 map = mapper.readValue(json, new TypeReference<Map<String, String>>() {
                 });
-                if(userService.login(map.get("username"), map.get("password"))) {
+                if (userService.login(map.get("username"), map.get("password"))) {
                     return new ResponseMessage("Login Succeeded!");
                 }
                 return new ResponseMessage("Login Failed");
@@ -80,5 +112,22 @@ class UserController {
                 return new ResponseMessage("Login Failed");
             }
         }, json());
+
+        delete("/unfollow/:id", (req, res) -> {
+            int id = Integer.parseInt(req.params(":id"));
+            if (userService.unfollowUser(id)) {
+                return new ResponseMessage(String.format("User unfollowed", id));
+            } else {
+                res.status(400);
+                return new ResponseMessage(String.format("Could not unfollow user", id));
+            }
+        }, json());
+
+
+
     }
+
+
+
 }
+
